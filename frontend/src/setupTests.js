58b88ff1fb1ccfa05a72111fc84c07579ca52193
Soleha Odilova <odilovasoleha73@gmail.com>
2025-08-24 02:03:13 +0300

    fodgram5

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// src/setupTests.js
+global.clearImmediate = global.clearImmediate || ((id) => clearTimeout(id));
+global.setImmediate = global.setImmediate || ((fn) => setTimeout(fn, 0));

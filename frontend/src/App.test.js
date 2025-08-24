99f2e89fd4686ce09e02198d7600a9f6c369b0a2
Soleha Odilova <odilovasoleha73@gmail.com>
2025-08-24 11:07:14 +0300

    Update workflow to skip failing tests4

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+      </Router>
+    );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
